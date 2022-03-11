b56d1ae3e3655868d06daa70007c4d120b6d7d77
Brennan Angel <brennan.angel@gmail.com>
2022-03-11 09:10:26 -0600

    simplify tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,6 @@
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.md'],
-  theme: {
-    extend: {
-        typography: {
-            DEFAULT: {
-                css: {
-                    "code::before": {content: ''},
-                    "code::after": {content: ''}
-                }
-            }
-        }
-    }
-},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  corePlugins: {
+    preflight: false,
+  }
 }
